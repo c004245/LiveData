3ff47f1826eec83d71683a97cdcb935d678f8901
hyunwook <c004245@naver.com>
2019-07-25 16:23:29 +0900

    단어 항목 추가 화면 추가 + 기능 구성 primary key 충돌 시 에러처리 java 8

--- app/src/main/java/hyunwook/co/kr/paging_livedata/MainActivity.java
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/MainActivity.java
@@ -1,15 +1,17 @@
 package hyunwook.co.kr.paging_livedata;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import hyunwook.co.kr.paging_livedata.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import hyunwook.co.kr.paging_livedata.entity.Word;
-import hyunwook.co.kr.paging_livedata.entity.WordListAdapter;
+import hyunwook.co.kr.paging_livedata.adapter.WordListAdapter;
 import hyunwook.co.kr.paging_livedata.viewmodel.WordViewModel;
 
 import java.util.List;
@@ -18,15 +20,20 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     RecyclerView mRecyclerView;
+    FloatingActionButton floatingActionButton;
     private WordViewModel mWordViewModel;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        floatingActionButton = findViewById(R.id.floatingBtn);
+        mRecyclerView = findViewById(R.id.recyclerView);
         final WordListAdapter adapter = new WordListAdapter(this);
 
         mRecyclerView.setAdapter(adapter);
@@ -34,13 +41,27 @@ public class MainActivity extends AppCompatActivity {
 
         mWordViewModel = ViewModelProviders.of(this).get(WordViewModel.class);
 
-        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
-            @Override
-            public void onChanged(List<Word> words) {
-                Log.d(TAG, "onChange!");
-                adapter.setWords(words);
-            }
+        mWordViewModel.getAllWords().observe(this, words -> {
+            Log.d(TAG, "onChange!");
+            adapter.setWords(words);
         });
+
+        floatingActionButton.setOnClickListener(l -> {
+            Intent intent = new Intent(this, NewWordActivity.class);
+            startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.empty_not_saved, Toast.LENGTH_LONG).show();
+        }
     }
 
 }
--- app/src/main/java/hyunwook/co/kr/paging_livedata/adapter/WordListAdapter.java
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/adapter/WordListAdapter.java
@@ -1,13 +1,13 @@
-package hyunwook.co.kr.paging_livedata.entity;
+package hyunwook.co.kr.paging_livedata.adapter;
 
 import android.content.Context;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import hyunwook.co.kr.paging_livedata.R;
+import hyunwook.co.kr.paging_livedata.entity.Word;
 
 import java.util.List;
 
