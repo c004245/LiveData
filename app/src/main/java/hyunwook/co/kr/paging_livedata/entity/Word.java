5c9f1730a7ca5e698b35013b86e8d8d10e56f963
hyunwook <c004245@naver.com>
2019-07-25 11:35:46 +0900

    add Room Annotate the Word class

--- app/src/main/java/hyunwook/co/kr/paging_livedata/entity/Word.java
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/entity/Word.java
@@ -1,8 +1,16 @@
 package hyunwook.co.kr.paging_livedata.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+@Entity(tableName = "word_table")
 public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
     private String mWord;
 
     private Word(@NonNull String word) {
