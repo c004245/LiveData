ba0ee34e8a10f0153aef8af6a9100cae90db19d0
hyunwook <c004245@naver.com>
2019-07-25 12:44:46 +0900

    Create Repository

--- app/src/main/java/hyunwook/co/kr/paging_livedata/room/WordRoomDatabase.java
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/room/WordRoomDatabase.java
@@ -13,7 +13,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
     public abstract WordDao wordDao();
     private static WordRoomDatabase INSTANCE;
 
-    static WordRoomDatabase getDatabase(final Context context) {
+    public static WordRoomDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (WordRoomDatabase.class) {
                 if (INSTANCE == null) {
