d7c5713d58467465d61d7ae8d14c7feb7f68d540
hyunwook <c004245@naver.com>
2019-07-29 15:43:11 +0900

    adding words activity convert to kotlin

---
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/NewWordActivity.kt
@@ -0,0 +1,34 @@
+package hyunwook.co.kr.paging_livedata
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new_word.*
+
+class NewWordActivity : AppCompatActivity() {
+
+    companion object {
+        val EXTRA_REPLY = "com.example.android.roomwordsample.REPLY"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_new_word)
+
+        button_save.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(edit_word.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = edit_word.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+}
