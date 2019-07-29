38a8da8b61ab075c195abefe37601e56faab77fb
hyunwook <c004245@naver.com>
2019-07-29 11:50:16 +0900

    Adapter and adding the RecyclerView convert to kotlin

---
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/adapter/WordListAdapter.kt
@@ -0,0 +1,45 @@
+package hyunwook.co.kr.paging_livedata.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hyunwook.co.kr.paging_livedata.R
+import hyunwook.co.kr.paging_livedata.entity.Word
+
+class WordListAdapter constructor(context: Context): RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private val mInflater = LayoutInflater.from(context)
+    private lateinit var mWords: List<Word>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        if (mWords != null) {
+            val current = mWords[position]
+            holder.wordItemView.text = current.word
+        } else {
+            holder.wordItemView.text = "No Word"
+        }
+    }
+
+    fun setWords(words: List<Word>) {
+        mWords = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() : Int {
+        return if (mWords != null)
+            mWords.size
+        else 0
+    }
+
+    inner class WordViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView = itemView.findViewById<TextView>(R.id.textView)!!
+    }
+}
