07d9fa86c6e9b0a769fa58ff86da18e552bfccf9
hyunwook <c004245@naver.com>
2019-08-06 18:11:37 +0900

    미사용 클래스 주석

--- app/src/main/java/hyunwook/co/kr/paging_livedata/UserProfileFragment.kt
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/UserProfileFragment.kt
@@ -1,3 +1,4 @@
+/*
 package hyunwook.co.kr.paging_livedata
 
 import android.os.Bundle
@@ -5,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import hyunwook.co.kr.paging_livedata.viewmodel.UserProfileViewModel
 
@@ -20,9 +22,13 @@ class UserProfileFragment : Fragment() {
         val userId = arguments?.getString(UID_KEY)
         viewModel = ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
         viewModel.init(userId)
+        viewModel.getUser().observe(this, Observer {
+            user -> {
+        }
+        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.user_profile, container, false)
+        return inflater.inflate(R.layout.recyclerview_item, container, false)
     }
-}
+}*/
--- app/src/main/java/hyunwook/co/kr/paging_livedata/viewmodel/UserProfileViewModel.kt
+++ app/src/main/java/hyunwook/co/kr/paging_livedata/viewmodel/UserProfileViewModel.kt
@@ -1,18 +1,20 @@
+/*
 package hyunwook.co.kr.paging_livedata.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import hyunwook.co.kr.paging_livedata.entity.User
 
 class UserProfileViewModel : ViewModel() {
 
     private var userId: String? = null
-    private lateinit var user: User
+    private lateinit var user: LiveData<User>
 
     fun init(userId: String?) {
         this.userId = userId
     }
 
-    fun getUser(): User {
+    fun getUser(): LiveData<User> {
         return user
     }
-}
+}*/
